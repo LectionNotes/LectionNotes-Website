c63ece3645405fccd2c431d78c67f654e0ac4337
I-IvanM <i_mikhno@icloud.com>
2025-07-17 00:12:47 +0300

    Добавили сервер с переменной
    Скорее всего в данной версии сервер надо будет запостить где-то ещё, а не в GitHub.

---
+++ server.py
@@ -0,0 +1,20 @@
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)  # Allow cross-origin requests for local development
+
+counter = 0
+
+@app.route('/counter', methods=['GET'])
+def get_counter():
+    return jsonify({'counter': counter})
+
+@app.route('/counter', methods=['POST'])
+def increment_counter():
+    global counter
+    counter += 1
+    return jsonify({'counter': counter})
+
+if __name__ == '__main__':
+    app.run(debug=True)
